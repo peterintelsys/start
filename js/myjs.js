4323833c1c5fce866ac5c34cf5e02724b45a7491
Peter <sppaulsson@gmail.com>
2019-10-09 10:34:39 +0200

    Update

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,66 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+class Ui {
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+  // close all open that are normaly hidden
+  // takes argument which collection
+  static closeShow(mycollection){
+        var i;
+        for (i = 0; i < mycollection.length; i++){
+    
+            mycollection[i].classList.remove("show");
+        }
+    }
+
+
+}
+
+
+const company = 'Intelsys';
+
+const name = document.querySelector('#companyname');
+
+name.textContent = company;
+
+const acc = document.querySelectorAll(".accordion");
+const lists = document.querySelectorAll('li');
+const panels = document.querySelectorAll(".panel");
+const dropBtn = document.querySelectorAll(".drop-btn");
+const drops = document.querySelectorAll(".dropdown");
+const dropdowns = document.querySelectorAll(".dropdown-content");
+const openD = document.querySelectorAll(".show");
+
+
+window.addEventListener("click", function(e){
+
+  
+    if(!e.target.matches(".drop-btn") && !e.target.matches(".drop-item")){
+      
+       Ui.closeShow(dropdowns);
+       
+      }
+});
+
+
+dropBtn.forEach(showDrop);
+
+function showDrop(drop) {
+  drop.addEventListener("click", function(e){
+
+
+    e.target.nextElementSibling.classList.toggle("show");
+
+    
+     
+  });
+}
+
+
+
+acc.forEach(openAcc);
+
+function openAcc(item){
+  item.addEventListener("click", function(e) {
+
+    var panel = e.target.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
@@ -12,6 +68,8 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+
 
 function openNav(a){
 	
@@ -22,3 +80,28 @@ function openNav(a){
   	}
  
 }
+
+
+lists.forEach(hoverColor);
+
+function hoverColor(list){
+  list.addEventListener('click', green);
+  list.addEventListener('mouseover', grey);
+  list.addEventListener('mouseout', white);
+
+    function grey(){
+    this.style.backgroundColor = '#f4f4f4';
+  }
+    function white(){
+    this.style.backgroundColor = '#fff';
+  }
+    function green(){
+    this.style.backgroundColor = '#d1e0b8';
+  }
+}
+
+function hide(){
+  this.style.display = 'none';
+}
+
+
